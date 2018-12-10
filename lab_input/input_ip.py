d6df25a599ff19e4825b5e9f54b8e0032c02e217
r hunter <hunter@javalina.net>
2018-12-10 15:46:09 -0500

    Added lab 7 script

---
+++ lab_input/input_ip.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created 12/10/2018
+# r hunter
+
+# ask for an IP address
+user_input = input('Please enter an IPv4 IP address:')
+
+# don't check anything
+# spit it back out
+print("You told me the IPv4 address is:" + user_input)
+
+# ask for vendor name
+vendorname = input('Please enter the vendor who supports it:')
+
+# return vendor name
+print("Here's a quarter; call someone who cares, maybe " + vendorname)
